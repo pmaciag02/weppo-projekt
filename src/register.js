867a4b0814a9075910ff9996e1a0c26a444583f0
stanely1 <sb.reda456@gmail.com>
2024-01-21 18:02:48 +0100

    disable creating user with empty name of password

--- src/register.js
+++ src/register.js
@@ -1,15 +1,20 @@
 const pool = require('./db')
 
 function register(req, res) {
-    let login = req.body.login.toString();
-    let password = req.body.password.toString();
+    const login = req.body.login.toString();
+    const password = req.body.password.toString();
+
+    if (!login || !password) {
+        res.render('signin', {login: req.session.valid, admin: req.session.admin, error: 'empty', signed: false});
+        return;
+    }
 
     (async function main() {
         try {
             var result = await pool.query("SELECT * FROM users WHERE username = \'" + login + "\'");
 
             if (result.rows.length > 0) {
-                res.render('signin', {login: req.session.valid, admin: req.session.admin, error: true, signed: false});
+                res.render('signin', {login: req.session.valid, admin: req.session.admin, error: 'taken', signed: false});
             }
             else {
                 await pool.query(`INSERT INTO users (username, password) VALUES ('${login}', '${password}')`);
